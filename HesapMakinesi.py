fd81c0cbd75d56910b6a412517d4b6d68d56764a
KAlpAkbaba <36965029+KAlpAkbaba@users.noreply.github.com>
2018-03-02 16:47:56 +0300

    Update HesapMakinesi.py

--- HesapMakinesi.py
+++ HesapMakinesi.py
@@ -1,25 +1,33 @@
-s1 = int(input("1. Sayiyi Giriniz : "))
-s2 = int(input("2. Sayiyi Giriniz : "))
-print("")
-print("---------------------------------")
-print("-- Toplama Islemi Icin (+) --")
-print("-- Cikarma Islemi Icin (-) --")
-print("-- Carpma Islemi Icin (*) --")
-print("-- Bolme Islemi Icin (/) --")
-print("---------------------------------")
-print("")
-islem=str(input("Yapilacak Islemi Giriniz : "))
-if islem =="+":
-    snc=s1+s2
-    print("Cevabınız :",snc)
-elif islem =="-":
-    snc=s1-s2
-    print("Cevabınız :",snc)
-elif islem =="*":
-    snc=s1*s2
-    print("Cevabınız :",snc)
-elif islem =="/":
-    snc=s1/s2
-    print("Cevabınız :",snc)
-else :
-    print("Islem Harfi Gecersiz ...")
+while True:
+    s1 = int(input("1. Sayiyi Giriniz : "))
+    s2 = int(input("2. Sayiyi Giriniz : "))
+    print("")
+    print("---------------------------------")
+    print("-- Toplama Islemi Icin (+) --")
+    print("-- Cikarma Islemi Icin (-) --")
+    print("-- Carpma Islemi Icin (*) --")
+    print("-- Bolme Islemi Icin (/) --")
+    print("---------------------------------")
+    print("")
+    islem = str(input("Yapilacak Islemi Giriniz : "))
+    if islem == "+":
+        snc = s1 + s2
+        print("Cevabınız :", snc)
+    elif islem == "-":
+        snc = s1 - s2
+        print("Cevabınız :", snc)
+    elif islem == "*":
+        snc = s1 * s2
+        print("Cevabınız :", snc)
+    elif islem == "/":
+        if s1 == 0 or s2 == 0:
+            print("")
+            print("0 a rakam bölünmez")
+        elif s1 != 0 or s2 != 0:
+            snc = s1 / s2
+            print("Cevabınız :", snc)
+
+    elif islem == "exit":
+        exit()
+    else:
+        print("Islem Harfi Gecersiz ...")
