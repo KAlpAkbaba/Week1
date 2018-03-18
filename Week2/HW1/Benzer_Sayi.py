3bb746515aec9bc0a7b938149abac935bd012908
KAlpAkbaba <36965029+KAlpAkbaba@users.noreply.github.com>
2018-03-18 17:04:00 +0300

    Update Benzer_Sayi.py

--- Week2/HW1/Benzer_Sayi.py
+++ Week2/HW1/Benzer_Sayi.py
@@ -1,18 +1,16 @@
 import random
-s = 0
-arr = [None] * 100
-for i in range(1, 100):
-
-    arr[i] = i
-
-
-arr[0] = random.randint(1, 99)
+s=0
+temp=[None]*100
+arr=[None]*99
+for i in range (1,100) :
+    arr[i]=i
+arr[0]=random.randint(1,99)
 print(arr)
 random.shuffle(arr)
-toplam = 0
 
-for i in arr :
-    toplam = toplam + i
-
-sayi = toplam - 4950
-print ("random sayi :",sayi)
+for k in arr:
+    c=temp[k-1]
+    if c == None :
+        temp[k-1]= 1
+    else:
+        print("Duplicate Sayı :",k)
