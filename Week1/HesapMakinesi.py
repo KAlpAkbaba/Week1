4d0e1bba33a062d45731727b73442bb8ce1624d4
KAlpAkbaba <36965029+KAlpAkbaba@users.noreply.github.com>
2018-03-18 16:24:22 +0300

    Update HesapMakinesi.py

--- Week1/HesapMakinesi.py
+++ Week1/HesapMakinesi.py
@@ -23,6 +23,12 @@ while True:
         if s1 == 0 or s2 == 0:
             print("")
             print("0 a rakam bölünmez")
+        if s2 > s1.
+            print("")
+            print("İkinci Sayı Birinci Sayıdan Büyük Olduğu İçin Bölme İşlemi Yapılamaz.")
+        if s1 == 0 and s2 > s1
+            print("")
+            print("0 'a Sayı Bölünmez")
         elif s1 != 0 or s2 != 0:
             snc = s1 / s2
             print("Cevabınız :", snc)
